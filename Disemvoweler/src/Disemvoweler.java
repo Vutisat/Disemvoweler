1ac30aaf8709961a552d9251ff8fbefd9dd4e076
Vutisat <vutisat@miamioh.edu>
2014-05-23 04:30:29 -0400

    Challenge is completed.

--- Disemvoweler/src/Disemvoweler.java
+++ Disemvoweler/src/Disemvoweler.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Disemvoweler {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Testing git");
+		
+		//Reads in user input
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter what your string to be disemvowel");
+		String input = in.nextLine();
+		in.close();
+		
+		input = input.replace(" ", "");
+		System.out.println("Result: ");
+		System.out.println(input.replaceAll("[a e i o u]", ""));
+		System.out.println("Vowels: ");
+		System.out.println(input.replaceAll("[b c d f g h j k l m n p q r s t v w x y z]", ""));
 	}
 
 }
