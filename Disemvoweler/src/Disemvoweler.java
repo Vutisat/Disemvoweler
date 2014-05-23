5ced7353d903af782c0e79a03a11ba8a304bfa7f
Vutisat <vutisat@miamioh.edu>
2014-05-23 03:35:14 -0400

    "First commit, testing github in Eclipse"

---
+++ Disemvoweler/src/Disemvoweler.java
@@ -0,0 +1,11 @@
+public class Disemvoweler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing git");
+	}
+
+}
